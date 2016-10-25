efd6b59462db0be72831ce010b65c14f63dc8371
Pranjal Satija <pranjal@ephemeraapp.xyz>
2016-10-25 11:14:44 -0400

    updated locations

--- Places.swift
+++ Places.swift
@@ -27,7 +27,7 @@ import MapKit
         for item in array {
             let dictionary = item as? [String : Any]
             let title = dictionary?["title"] as? String
-            let subtitle = dictionary?["subtitle"] as? String
+            let subtitle = dictionary?["description"] as? String
             let latitude = dictionary?["latitude"] as? Double ?? 0, longitude = dictionary?["longitude"] as? Double ?? 0
             
             let place = Place(title: title, subtitle: subtitle, coordinate: CLLocationCoordinate2DMake(latitude, longitude))
